3d0c014664df354706c33711ca25c8355fd772ea
Will Daniels <danwil02@hotmail.com>
2015-11-27 19:11:41 +1030

    Initial commit - singleton work

---
+++ DesignPatternNotes/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternNotes
+{
+    // Class is not threadsafe (due to timing)
+    // Instance methods generally aren't threadsafe
+    public class Singleton
+    {
+        private static class SingletonHolder
+        {
+            internal static readonly Singleton instance = new Singleton();
+            // Empty static constructor - forces laziness
+            static SingletonHolder() { }
+        }
+
+        public static Singleton Instance { get { return SingletonHolder.instance; } }
+
+        private Singleton()
+        {
+            // Stuff that must only happen once
+            Console.WriteLine("Singleton Constructor");
+        }
+
+        public static void SayHi()
+        {
+            Console.WriteLine("Hi There");
+        }
+
+        // Raison d'etre for the class
+        public void DoSomething()
+        {
+            // This must be thread-safe!
+            Console.WriteLine("Do something");
+        }
+    }
+}
---
+++ DesignPatternNotes/SingletonTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternNotes
+{
+    [TestClass]
+    public class SingletonTest
+    {
+        [TestMethod]
+        public void UseSingleton()
+        {
+            Singleton.SayHi();
+            Console.WriteLine("Start of test");
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+
+            Assert.AreSame(s1, s2);
+        }
+        
+    }
+}
